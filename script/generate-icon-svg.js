7dd4da9900d016753bc8abed5e47a748b26dc521
Michal Nazarewicz <mina86@mina86.com>
2026-03-26 15:35:46 +0100

    logos: refactor generate-icon-svg.js script
    Group things into functions and rearrange code a little.  Introduces
    no behaviour changes to the script.  The changes are to help with
    additions to the script in upcoming commits.
    
    Signed-off-by: Michał Nazarewicz <mina86@mina86.com>

--- script/generate-icon-svg.js
+++ script/generate-icon-svg.js
@@ -16,60 +16,86 @@
 
 const paper = require('paper');
 const { PaperOffset } = require('paperjs-offset');
+
 const fs = require('fs');
 const path = require('path');
-
-paper.setup(new paper.Size(78, 78));
-
-// Background: rounded rectangle
-const bg = new paper.Path.Rectangle({
-  point: [0, 0],
-  size: [58, 58],
-  radius: 5,
-});
-
-// Branch lines (expand strokes into filled outlines)
-const mainBranch = new paper.Path.Line({ from: [40.5, 0], to: [40.5, 41] });
-const mainExp = PaperOffset.offsetStroke(mainBranch, 2.5, { cap: 'butt' });
-mainBranch.remove();
-
-const topicBranch = new paper.Path.Line({ from: [40.5, 18], to: [17.5, 41] });
-const topicExp = PaperOffset.offsetStroke(topicBranch, 2.5, { cap: 'butt' });
-topicBranch.remove();
-
-// Circles
-const branchPoint = new paper.Path.Circle({ center: [40.5, 18], radius: 6 });
-const mainStart   = new paper.Path.Circle({ center: [40.5, 41], radius: 6 });
-const topicStart  = new paper.Path.Circle({ center: [17.5, 41], radius: 6 });
-
-// Unite all graph elements
-let graph = mainExp.unite(topicExp);
-graph = graph.unite(branchPoint);
-graph = graph.unite(mainStart);
-graph = graph.unite(topicStart);
-
-// Subtract graph from background
-const icon = bg.subtract(graph);
-
-const pathData = icon.pathData;
-
-const transform = `translate(10 10) rotate(-45 29 29)`;
-
-const colors = {
-  '1788C': '#f03c2e',
-  'Black': '#100f0d',
-  'White': '#fff',
-};
-
 const outDir = path.join(__dirname, '../static/images/logos/downloads');
 
-for (const [variant, fill] of Object.entries(colors)) {
+function saveFile(filename, data) {
+  const outPath = path.join(outDir, filename);
+  fs.writeFileSync(outPath, data);
+  console.log(`Wrote ${outPath}`);
+}
+
+
+// ================================== Icon ===================================
+
+// The path data of the git icon.
+const iconGlyph = (() => {
+  paper.setup(new paper.Size(78, 78));
+
+  // Background: rounded rectangle
+  const bg = new paper.Path.Rectangle({
+    point: [0, 0],
+    size: [58, 58],
+    radius: 5,
+  });
+
+  // Branch lines (expand strokes into filled outlines)
+  const mainBranch = new paper.Path.Line({ from: [40.5, 0], to: [40.5, 41] });
+  const mainExp = PaperOffset.offsetStroke(mainBranch, 2.5, { cap: 'butt' });
+  mainBranch.remove();
+
+  const topicBranch = new paper.Path.Line({ from: [40.5, 18], to: [17.5, 41] });
+  const topicExp = PaperOffset.offsetStroke(topicBranch, 2.5, { cap: 'butt' });
+  topicBranch.remove();
+
+  // Circles
+  const branchPoint = new paper.Path.Circle({ center: [40.5, 18], radius: 6 });
+  const mainStart   = new paper.Path.Circle({ center: [40.5, 41], radius: 6 });
+  const topicStart  = new paper.Path.Circle({ center: [17.5, 41], radius: 6 });
+
+  // Unite all graph elements
+  let graph = mainExp.unite(topicExp);
+  graph = graph.unite(branchPoint);
+  graph = graph.unite(mainStart);
+  graph = graph.unite(topicStart);
+
+  // Subtract graph from background
+  const icon = bg.subtract(graph);
+
+  return icon.pathData;
+})();
+
+// Transform which converts the 58×58 square into a diamond (i.e. rotates it by
+// 45° and centres it within 78×78 box).
+const iconTransform = `translate(10 10) rotate(-45 29 29)`;
+
+// Generates and saves the icon image file.
+function generateIcon(variant, iconFill) {
   const svg =
     `<svg xmlns="http://www.w3.org/2000/svg" width="92pt" height="92pt"` +
-    ` viewBox="0 0 78 78"><path fill="${fill}"` +
-    ` transform="${transform}" d="${pathData}"/></svg>`;
+    ` viewBox="0 0 78 78"><path fill="${iconFill}"` +
+    ` transform="${iconTransform}" d="${iconGlyph}"/></svg>`;
+  saveFile(`Git-Icon-${variant}.svg`, svg);
+}
 
-  const outPath = path.join(outDir, `Git-Icon-${variant}.svg`);
-  fs.writeFileSync(outPath, svg);
-  console.log(`Wrote ${outPath}`);
+
+// =========================== Variant definitions ===========================
+
+const orange = '#f03c2e';
+const black  = '#100f0d';
+const white  = '#fff';
+
+const iconVariants = {
+  '1788C': { iconFill: orange },
+  'Black': { iconFill: black },
+  'White': { iconFill: white },
+};
+
+
+// ================================ Generate =================================
+
+for (const [variant, { iconFill }] of Object.entries(iconVariants)) {
+  generateIcon(variant, iconFill);
 }
